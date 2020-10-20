0ca59602cce358fe5f26b836f15c1991f3b42bde
sohaibahmed553 <57675425+sohaibahmed553@users.noreply.github.com>
2020-10-20 19:46:54 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const cors = require("cors");
+
+const schema = require("./schema");
+
+const app = express();
+
+//Allow cross-origin
+app.use(cors());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started at ${PORT}`));
